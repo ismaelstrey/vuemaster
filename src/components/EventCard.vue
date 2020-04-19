802e1238465efdf440e76cf503a37c6c91edd504
ismael strey <ismaelstrey@hotmail.com>
2020-04-18 22:37:38 -0300

    initial commit

---
+++ src/components/EventCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>Vcard</h2>
+    <ul>
+      <li class="teste">teste</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EventCard"
+};
+</script>
+
+<style scoped>
+.teste {
+  color: aquamarine;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2>
+    {{ msg }}
+    <EventCard />
+  </h2>
+</template>
+
+<script>
+import EventCard from "./EventCard";
+export default {
+  name: "HelloWorld",
+  components: EventCard,
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
